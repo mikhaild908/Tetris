c83aef66e5091a9e9c9cc9d7594c851dac2387e1
Mike Dumlao <mikhaild908@gmail.com>
2020-05-30 08:32:38 -0700

    fixed formatting

--- Tetris/STetromino.cs
+++ Tetris/STetromino.cs
@@ -1,8 +1,35 @@
-﻿using System;
+using System;
 namespace Tetris
 {
     public class STetromino : Tetromino
     {
-        public STetromino()         {             _firstPosition = new bool[][]             {                 new bool [] { false, true, true },// false } ,                 new bool [] { true, true, false }// false } ,                 //new bool [] { false, false, false, false } ,                 //new bool [] { false, false, false, false }            };              _secondPosition = new bool[][]             {                 new bool [] { true, false }, // false, false} ,                 new bool [] { true, true}, // false, false } ,                 new bool [] { false, true} // false, false } ,                 //new bool [] { false, false, false, false }             };              _thirdPosition = new bool[][]             {                 new bool [] { false, true, true }, // false} ,                 new bool [] { true, true, false } // false } ,                 //new bool [] { false, false, false, false } ,                 //new bool [] { false, false, false, false }             };              _fourthPosition = new bool[][]             {                 new bool [] { true, false }, // false, false} ,                 new bool [] { true, true }, // false, false } ,                 new bool [] { false, true }, // false, false } ,                 //new bool [] { false, false, false, false }             };         } 
+        public STetromino()
+        {
+            _firstPosition = new bool[][]
+            {
+                new bool [] { false, true, true },
+                new bool [] { true, true, false }
+            };            
+                
+            _secondPosition = new bool[][]
+            {
+                new bool [] { true, false },
+                new bool [] { true, true},
+                new bool [] { false, true}
+            };
+            
+            _thirdPosition = new bool[][]
+            {
+                new bool [] { false, true, true }, 
+                new bool [] { true, true, false }
+            };
+            
+            _fourthPosition = new bool[][]
+            {
+                new bool [] { true, false },
+                new bool [] { true, true },
+                new bool [] { false, true }
+            };
+        }
     }
 }
